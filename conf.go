5d68853724eb99c310c5d8fce02b6b1f311e6cbd
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-08 20:20:08 -0600

    Create conf.go

---
+++ conf.go
@@ -0,0 +1,32 @@
+package arotw
+
+import (
+	"time"
+)
+
+type Conf struct {
+	MaxFilesize    int
+	NumFilesToKeep int
+	BufferSize     int
+	UseGzip        bool
+	Prefix         string
+	Suffix         string
+}
+
+func NewConf() Conf {
+	return Conf{
+		MaxFilesize:    MB,
+		NumFilesToKeep: 5,
+		BufferSize:     -1,
+		Prefix:         "alw",
+		Suffix:         ".log",
+	}
+}
+
+func (c Conf) symlink() string {
+	return c.Prefix + c.Suffix
+}
+
+func (c Conf) newFilename() string {
+	return c.Prefix + "-" + time.Now().Format("20060102-150405") + c.Suffix
+}
