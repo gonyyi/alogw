a4be5c8322d4027b322cfb9967491d8a0d29d7f2
Gon <29622355+gonyyi@users.noreply.github.com>
2021-04-06 12:07:15 -0500

    Create test.go

---
+++ cmd/test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonyyi/alogw"
+	"time"
+)
+
+func main() {
+	c := alogw.NewConf()
+	c.EnableGzip = true
+	c.FileAggrLevel = alogw.BySecond
+	c.MaxSize = alogw.KB * 1
+	c.Prefix = "./data/test-"
+	c.Suffix = ".log"
+	w, err := alogw.NewWriter(c)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	for i := 0; i < 1_000_000; i++ {
+		w.Write([]byte(fmt.Sprintf("time: %s, count: %d\n", time.Now().String(), i)))
+	}
+	w.Close()
+}
